e52e27ec29ceccd7317fac2fd3e81fed6ac9d3bb
CaptCorpMURICA <kevin.zufelt@gmail.com>
2023-01-30 17:50:43 -0500

    Updated script to add additional parts of the exercise previously omitted.

--- Udemy/Snowflake/CompleteMasterclass/Section02.sql
+++ Udemy/Snowflake/CompleteMasterclass/Section02.sql
@@ -12,3 +12,37 @@ GRANT imported privileges ON DATABASE snowflake_sample_data TO ROLE PUBLIC;
 
 -- Query a table in the sample data.
 SELECT * FROM snowflake_sample_data.tpch_sf1.customer;
+
+-- Create a database
+CREATE DATABASE FIRST_DB;
+
+-- Rename database & create the table & metadata
+ALTER DATABASE FIRST_DB RENAME TO OUR_FIRST_DB;
+
+CREATE TABLE OUR_FIRST_DB.PUBLIC.LOAN_PAYMENT (
+    LOAD_ID STRING,
+    LOAN_STATUS STRING,
+    PRINCIPAL STRING,
+    TERMS STRING,
+    EFFECTIVE_DATE STRING,
+    DUE_DATE STRING,
+    PAID_OFF_TIME STRING,
+    PAST_DUE_DAYS STRING,
+    AGE STRING,
+    EDUCATION STRING,
+    GENDER STRING
+);
+
+-- Check that the table is empty
+USE DATABASE OUR_FIRST_DB;
+SELECT * FROM LOAN_PAYMENT;
+
+-- Loading the data from S3 bucket
+COPY INTO LOAN_PAYMENT
+    FROM s3://bucketsnowflakes3/Loan_payments_data.csv
+    file_format = (type = csv
+                   field_delimiter = ','
+                   skip_header = 1);
+
+-- Validate
+SELECT * FROM LOAN_PAYMENT;
