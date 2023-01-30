200311f3a8893846ac57ff7bc6c48a5ffec57eff
CaptCorpMURICA <kevin.zufelt@gmail.com>
2023-01-30 16:41:50 -0500

    Corrected spelling and added explicit declaration for warehouse and role.

--- Udemy/Snowflake/CompleteMasterclass/Section02.sql
+++ Udemy/Snowflake/CompleteMasterclass/Section02.sql
@@ -1,9 +1,14 @@
--- Create a database from the share.
-CREATE DATABASE snowflake_sample_data FROM SHARE sfc-samples.sample_data;
+-- Declare Warehouse and Role
+USE WAREHOUSE LEOPARD_WH;
+USE ROLE ACCOUNTADMIN;
 
--- Grant the PUBLIC role access to the databse.
+-- Create a database from the share.
+-- DROP DATABASE snowflake_sample_data;
+CREATE DATABASE snowflake_sample_data FROM SHARE sfc_samples.sample_data;
+
+-- Grant the PUBLIC role access to the database.
 -- Optionally change the role name to restrict access to a subset of users.
-GRANT umported privileges ON DATABASE snowflake_sample_data TO ROLE PUBLIC;
+GRANT imported privileges ON DATABASE snowflake_sample_data TO ROLE PUBLIC;
 
 -- Query a table in the sample data.
 SELECT * FROM snowflake_sample_data.tpch_sf1.customer;
